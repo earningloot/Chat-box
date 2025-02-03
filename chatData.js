2a95d3e948e4bcbc55647a464259aaf326ba5290
shivanshojha01 <shivanshojha101@gmail.com>
2025-02-03 23:39:28 +0530

    Create chatData.js

---
+++ chatData.js
@@ -0,0 +1,9 @@
+const chatData = {
+    "how many types of reactions are there": "There are 9 types of reactions.",
+    "types of reaction": "There are 9 types of reactions.",
+    "reaction types": "There are 9 types of reactions.",
+    "how many reactions are there": "There are 9 types of reactions.",
+    "can you tell me about reaction types": "There are 9 types of reactions.",
+    "what are the different reaction types": "There are 9 types of reactions.",
+    // Add more questions and answers here
+};
